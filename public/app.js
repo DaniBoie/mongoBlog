7461cf5ca30fd4ea89cc8a693814d50397c7c24d
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-09 14:39:18 -0700

    added pre-user functionality

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
 let userId
+let loginId
 
 document.getElementById('submit').addEventListener('click', () => {
   event.preventDefault()
   axios.post('/api/blogposts', {
     text: document.getElementById('makeBlog').value,
-    user: userId
+    user: loginId || userId
   })
     .then(function (response) {
       console.log(response)
@@ -24,6 +25,7 @@ document.getElementById('submitUser').addEventListener('click', () => {
   event.preventDefault()
   document.getElementById('blogEntry').style.display = ''
   document.getElementById('userEntry').style.display = 'none'
+  document.getElementById('login').style.display = 'none'
   axios.post('/api/users', {
     name: document.getElementById('name').value,
     username: document.getElementById('username').value,
@@ -35,4 +37,32 @@ document.getElementById('submitUser').addEventListener('click', () => {
     .catch(function (error) {
       console.log(error);
     });
+})
+
+document.getElementById('submitLogin').addEventListener('click', () => {
+  event.preventDefault()
+  axios.get('/api/users')
+    .then(res => {
+      res.data.forEach(element => {
+        // console.log(element.username)
+        if (document.getElementById('oldUsername').value === element.username) {
+          loginId = element._id
+          element.Blogposts.forEach(element => {
+            console.log(element)
+            let blogLi = document.createElement('li')
+            blogLi.innerHTML = `
+      <p>${element.text}</p>
+      `
+            document.getElementById('blogs').append(blogLi)
+          });
+          document.getElementById('blogEntry').style.display = ''
+          document.getElementById('userEntry').style.display = 'none'
+          document.getElementById('login').style.display = 'none'
+        }
+      });
+      console.log(res.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 })
