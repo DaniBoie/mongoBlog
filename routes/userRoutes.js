47786f0f5f7e74dd34925a78cc9946dd0ddcf6ce
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-10 13:27:33 -0700

    finished user authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 const { User } = require('../models')
+const jwt = require('jsonwebtoken')
 
+router.post('/users/register', (req, res) => {
+  const {name, email, username, password} = req.body
+  User.register(new User({name, email, username}), password, err => {
+    if (err) {
+    console.log(err)} res.sendStatus(200)
+  })
+})
 
+router.post('/users/login', (req, res) => {
+  const {username, password} = req.body
+  User.authenticate()(username, password, (err, user) => {
+    if(err) {console.log(err)}
+    res.json(user ? jwt.sign({ id: user._id}, process.env.SECRET) : null)
+  })
+})
 
 // GET all Users
 // router.get('/users', (req, res) => {
