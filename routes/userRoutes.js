d2581cdb2b4265f0001c4da86ef29759ef336782
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-11 11:43:10 -0700

    fixed comments in code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,38 +22,9 @@ router.post('/users/login', (req, res) => {
   })
 })
 
+// ROUTE to get the posts from a specific user
 router.get('/users/posts', passport.authenticate('jwt'), (req, res) => {
   res.json(req.user)
 })
-
-// GET all Users
-// router.get('/users', (req, res) => {
-//   User.find()
-//     .populate('Blogposts')
-//     .then(users => { res.json(users) })
-//     .catch(err => console.log(err))
-// })
-
-// POST one User
-// router.post('/users', (req, res) => {
-//   User.create(req.body)
-//     .then(user => res.json(user))
-//     .catch(err => console.log(err))
-// })
-
-// PUT one User
-// router.put('/users/:id', (req, res) => {
-//   User.findByIdAndUpdate(req.params.id, req.body)
-//     .then(() => res.sendStatus(200))
-//     .catch(err => console.log(err))
-// })
-
-// DELETE one User
-// router.delete('/users/:id', (req, res) => {
-//   User.findByIdAndDelete(req.params.id)
-//     .then(() => res.sendStatus(200))
-//     .catch(err => console.log(err))
-// })
-
 
 module.exports = router
