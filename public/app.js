11cf07d145914b7163090e1f0114f0e4869a14c2
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-11 11:26:53 -0700

    login authorization

--- public/app.js
+++ public/app.js
@@ -1,6 +1,19 @@
+axios.get('/api/users/posts', {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('user')}`
+  }
+})
+  .then(({data: user}) => {
+    console.log(user)
+  })
+  .catch(err => {
+    console.log(err)
+    window.location = '/auth.html'
+  })
+
 // Varibles used to check if someone is either logging in or creating an account
-let userId
-let loginId
+// let userId
+// let loginId
 
 // Event listener to create a blogpost
 document.getElementById('submit').addEventListener('click', () => {
@@ -8,7 +21,7 @@ document.getElementById('submit').addEventListener('click', () => {
   axios.post('/api/blogposts', {
     text: document.getElementById('makeBlog').value,
     user: loginId || userId
-  })
+  }, )
     .then(function (response) {
       console.log(response)
       let blogLi = document.createElement('li')
@@ -24,47 +37,47 @@ document.getElementById('submit').addEventListener('click', () => {
 })
 
 // Event listener to create a new user
-document.getElementById('submitUser').addEventListener('click', () => {
-  event.preventDefault()
-  document.getElementById('blogEntry').style.display = ''
-  document.getElementById('userEntry').style.display = 'none'
-  document.getElementById('login').style.display = 'none'
-  axios.post('/api/users', {
-    name: document.getElementById('name').value,
-    username: document.getElementById('username').value,
-    email: document.getElementById('email').value
-  })
-    .then(function (response) {
-      userId = response.data._id
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-})
+// document.getElementById('submitUser').addEventListener('click', () => {
+//   event.preventDefault()
+//   document.getElementById('blogEntry').style.display = ''
+//   document.getElementById('userEntry').style.display = 'none'
+//   document.getElementById('login').style.display = 'none'
+//   axios.post('/api/users', {
+//     name: document.getElementById('name').value,
+//     username: document.getElementById('username').value,
+//     email: document.getElementById('email').value
+//   })
+//     .then(function (response) {
+//       userId = response.data._id
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// })
 
 // Event listener for client login
-document.getElementById('submitLogin').addEventListener('click', () => {
-  event.preventDefault()
-  axios.get('/api/users')
-    .then(res => {
-      res.data.forEach(element => {
-        if (document.getElementById('oldUsername').value === element.username) {
-          loginId = element._id
-          element.Blogposts.forEach(element => {
-            console.log(element)
-            let blogLi = document.createElement('li')
-            blogLi.innerHTML = `
-      <p>${element.text}</p>
-      `
-            document.getElementById('blogs').append(blogLi)
-          });
-          document.getElementById('blogEntry').style.display = ''
-          document.getElementById('userEntry').style.display = 'none'
-          document.getElementById('login').style.display = 'none'
-        }
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-})
+// document.getElementById('submitLogin').addEventListener('click', () => {
+//   event.preventDefault()
+//   axios.get('/api/users')
+//     .then(res => {
+//       res.data.forEach(element => {
+//         if (document.getElementById('oldUsername').value === element.username) {
+//           loginId = element._id
+//           element.Blogposts.forEach(element => {
+//             console.log(element)
+//             let blogLi = document.createElement('li')
+//             blogLi.innerHTML = `
+//       <p>${element.text}</p>
+//       `
+//             document.getElementById('blogs').append(blogLi)
+//           });
+//           document.getElementById('blogEntry').style.display = ''
+//           document.getElementById('userEntry').style.display = 'none'
+//           document.getElementById('login').style.display = 'none'
+//         }
+//       });
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
+// })
